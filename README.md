bc4b475f0535536e21d82da2def64c18c05fbb04
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-10 06:07:10 +0530

    [Sankar]ADDED:Player Rolls the Dice to move forward.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 #### This problem simulates a Snake and Ladder Game. The player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.
 ### Used Case:1
 #### Snake and Ladder game played at start position 0.
+### Used Case:2
+#### Player Rolls the die to get a number between 1 to 6 using RANDOM Function.
--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class SnakeLadder {
 	public static void main(String[] args) {
-		int player1Position;
+		int player1Position = 0;
+		int diceRoll = (int)(Math.floor(Math.random()*10) % 6)+1;
 		//Creating a Scanner Obj for multiple uses
 		Scanner sc = new Scanner(System.in);
 		System.out.println("***** Welcome *****");
-		System.out.println("Do u wanna start the game?(Y/N): ");
+		System.out.println("Do you wanna start the game?(Y/N): ");
 		String choice = sc.nextLine();
 		if ( choice.equals ("y") || choice.equals ("Y") ) {
 			player1Position = 0;
-			System.out.println("Player1 Starts at Position 0");
+			System.out.println("Player1 Starts at Position "+player1Position);
 		}
-		else
+		else {
 			System.out.println("Sad to see you not play!! See Ya!");
+			System.exit(0);
+		}
+		System.out.println("Dice Rolled: "+diceRoll);
+                player1Position += diceRoll;
+                System.out.println("Player1 Postion "+player1Position);
+
 	}
 }
