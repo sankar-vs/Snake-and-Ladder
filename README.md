2114e0a10d71990bd7141e85c3ce8e2358d83248
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-10 05:59:29 +0530

    [Sankar]ADDED:Player-1 Starts the Game at Position 0.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Welcome to Snake Ladder Program
+#### This problem simulates a Snake and Ladder Game. The player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.
+### Used Case:1
+#### Snake and Ladder game played at start position 0.
--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class SnakeLadder {
 	public static void main(String[] args) {
+		int player1Position;
+		//Creating a Scanner Obj for multiple uses
+		Scanner sc = new Scanner(System.in);
 		System.out.println("***** Welcome *****");
+		System.out.println("Do u wanna start the game?(Y/N): ");
+		String choice = sc.nextLine();
+		if ( choice.equals ("y") || choice.equals ("Y") ) {
+			player1Position = 0;
+			System.out.println("Player1 Starts at Position 0");
+		}
+		else
+			System.out.println("Sad to see you not play!! See Ya!");
 	}
 }
